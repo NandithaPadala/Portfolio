e7c689489f75cb3019f031868652825c90952bc9
NandithaPadala <padalanandihta@gmail.com>
2025-04-26 13:28:09 +0530

    changes are reflected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { AboutComponent } from './components/about/about.component';
 import { EducationComponent } from './components/education/education.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HeroComponent } from './components/hero/hero.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SkillsComponent } from './components/skills/skills.component';
-
 
 @Component({
   selector: 'app-root',
@@ -17,7 +16,6 @@ import { SkillsComponent } from './components/skills/skills.component';
     NavbarComponent,
     FooterComponent,
     SkillsComponent,
-    HeroComponent,
     MatIconModule,
     EducationComponent,
     ExperienceComponent,
@@ -28,17 +26,21 @@ import { SkillsComponent } from './components/skills/skills.component';
 export class AppComponent {
   title = 'Portfolio';
 
-  ngOnInit() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetId = anchor.getAttribute('href')!.substring(1);
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-      });
-    });
-  }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // This code will only run in the browser
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', (e) => {
+          e.preventDefault();
+          const targetId = anchor.getAttribute('href')!.substring(1);
+          const targetElement = document.getElementById(targetId);
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+          }
+        });
+      });
+    }
+  }
 }
